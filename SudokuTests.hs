b89df30f7cb09b41e1e2b273a03e840c53831c53
David Fontenot <fontenod@onid.oregonstate.edu>
2014-08-09 15:27:13 -0700

    refactored tests, added sudoku reader tests split up test cases by sudoku reader and sudoku solver added clunky test generator that will read a file new dependencies on System.FilePath and Data.List

--- SudokuTests.hs
+++ SudokuTests.hs
@@ -1,15 +1,40 @@
 module SudokuTests where
 
 import Test.HUnit
+import Data.List
+import Data.Array
+import System.FilePath
 
 import qualified SudokuSolver as SS
 import qualified SudokuReader as SR
 
-pointInRangeTester p1 p2 tf = TestCase $ assertEqual ((show p1) ++ " " ++ (show p2)) tf $ SS.pointInRange p1 p2
+-- utility functions
+pathJoin dirs = intercalate [pathSeparator] dirs
 
+-- generates a test case
+pointInRangeTester p1 p2 tf = ((show p1) ++ " " ++ (show p2)) ~: tf ~=? (SS.pointInRange p1 p2)
+
+-- two basic test cases
 pointInRange1 = pointInRangeTester (0,0) (1,1) True
 pointInRange2 = pointInRangeTester (0,0) (8,7) False
 
-allSSTests = TestList [TestLabel "pointInRange1" pointInRange1, TestLabel "pointInRange2" pointInRange2]
+-- assert that opts must equal calculated options
+-- fn does not include directory
 
-main = do runTestTT allSSTests
+-- TODO: refactor, ugly
+optionsTests pt opts fn = "[Board " ++ fn ++ "] Testing options at " ++ (show pt) ~: 
+                          return (pathJoin ["boards", fn]) >>= readFile >>= 
+                          (\f -> case SR.readSudokuFile f of 
+                              Just board -> return $ (SS.options (assocs board) pt) == opts
+                              _ -> return False) >>= assertBool "Options didn't match"
+
+options1 = optionsTests (0,1) [1,3,4,6] "board1.txt"
+
+allSSTests = [TestLabel "pointInRange1" pointInRange1, 
+              TestLabel "pointInRange2" pointInRange2]
+
+allSRTests = [TestLabel "options1" options1]
+
+allTests = TestList (allSSTests ++ allSRTests)
+
+main = do runTestTT allTests
